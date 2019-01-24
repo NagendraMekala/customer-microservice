3395e18d6fd278e9ae608b0227791d79c392d079
NagendraMekala <nagendra.kldm@gmail.com>
2019-01-24 23:55:56 +0530

    updated logger handler code

--- gateway-service/src/main/java/com/mng/spring/main/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/mng/spring/main/GatewayServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GatewayServiceApplication {
 
-	//http://nagendra:8765/api/customer/customers/1
+	//http://localhost:8765/api/customer/customers/1
 	//http://nagendra:8765/api/customer/
 	//http://nagendra:8765/api/account/
 	public static void main(String[] args) {
