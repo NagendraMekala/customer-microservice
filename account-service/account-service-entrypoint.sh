74b276cc542a980c0a76d4501b7d40b835af2a14
NagendraMekala <nagendra.kldm@gmail.com>
2019-01-26 06:21:16 -0800

    docker files working

--- account-service/account-service-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-while ! nc -z discovery-service 8761 ; do
-    echo "Waiting for upcoming discovery-service"
-    sleep 3
-done
-
-java -jar /opt/lib/account-service-0.0.1-SNAPSHOT.jar
---
+++ docker-files/customer-service-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+while ! nc -z zipkin-server 9411 ; do
+    echo "Waiting for upcoming zipkin-server"
+    sleep 3
+done
+
+while ! nc -z discovery-service 8761 ; do
+    echo "Waiting for upcoming discovery-service"
+    sleep 3
+done
+
+while ! nc -z account-service 2222 ; do
+    echo "Waiting for upcoming account-service"
+    sleep 3
+done
+
+java -jar /opt/lib/customer-service-0.0.1-SNAPSHOT.jar
